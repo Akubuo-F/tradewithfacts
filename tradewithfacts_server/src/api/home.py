94871c2a88f73dc4832b29026a8fda9fde639367
Favour Chigoziri Akubuo <akubuof.work@gmail.com>
2024-10-29 18:42:17 +0100

    added rate limiting to home route

--- tradewithfacts_server/src/api/home.py
+++ tradewithfacts_server/src/api/home.py
@@ -1,11 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from slowapi import Limiter
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
 
 
-home_route = FastAPI()
+home = FastAPI()
 
-@home_route.get("/")
-async def root():
-    content: dict[str, str] = {"message": "Welcome to TradeWithFacts"}
+limiter = Limiter(key_func=get_remote_address)
+home.state.limiter = limiter
+
+home.add_exception_handler(
+    429,
+    lambda request, exception: JSONResponse(
+        content={"message": "Rate limit exceeded. Please try again later in 5 minutes."},
+        status_code=429
+    )
+)
+
+# noinspection PyTypeChecker
+home.add_middleware(SlowAPIMiddleware)
+
+
+@home.get("/")
+@limiter.limit("5/minute")
+async def root(request: Request):
+    content: dict[str, str] = {"message": "Welcome to TradeWithFacts!"}
     status_code: int = 200
     return JSONResponse(content, status_code)
+
+
+if __name__ == '__main__':
+    ...
