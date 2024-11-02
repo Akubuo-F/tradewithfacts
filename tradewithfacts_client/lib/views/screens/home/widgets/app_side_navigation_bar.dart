88c7d2bd0fda565479674af274d8602e468a55bb
Favour Chigoziri Akubuo <akubuof.work@gmail.com>
2024-11-02 16:58:22 +0100

    Worked on responsiveness
    App layout is now responsive on mobile, tablet, and desktop.

--- tradewithfacts_client/lib/views/screens/home/widgets/app_side_navigation_bar.dart
+++ tradewithfacts_client/lib/views/screens/home/widgets/app_side_navigation_bar.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tradewithfacts_client/views/screens/home/providers/app_navigation_bar_provider.dart';
 import 'package:tradewithfacts_client/views/screens/home/providers/page_display_manager_provider.dart';
+import 'package:tradewithfacts_client/views/utils/responsive_manager.dart';
 
-class AppBottomNavigationBar extends StatelessWidget {
-  const AppBottomNavigationBar({super.key});
+class AppSideNavigationBar extends StatelessWidget {
+  const AppSideNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: context.watch<AppNavigationBarProvider>().selectedItemIndex,
-      onTap: (selectedIndex) {
+    return NavigationRail(
+      selectedIndex:
+          context.watch<AppNavigationBarProvider>().selectedItemIndex,
+      onDestinationSelected: (selectedIndex) {
         context.read<AppNavigationBarProvider>().navigateTo(selectedIndex);
         context.read<PageDisplayManagerProvider>().display(selectedIndex);
       },
-      items: _buildItems(),
+      extended: ResponsiveManager.isDesktop(context),
+      selectedIconTheme: Theme.of(context).iconTheme,
+      selectedLabelTextStyle:
+          TextStyle(color: Theme.of(context).colorScheme.secondary),
+      destinations: _buildDestinations(context),
     );
   }
 
-  List<BottomNavigationBarItem> _buildItems() {
+  List<NavigationRailDestination> _buildDestinations(BuildContext context) {
     Map<int, Icon> icons = {
       0: const Icon(Icons.sentiment_very_satisfied),
       1: const Icon(Icons.public_rounded),
@@ -34,9 +40,9 @@ class AppBottomNavigationBar extends StatelessWidget {
     }
     return List.generate(
       icons.length,
-      (generatedIndex) => BottomNavigationBarItem(
+      (generatedIndex) => NavigationRailDestination(
         icon: icons[generatedIndex]!,
-        label: labels[generatedIndex],
+        label: Text(labels[generatedIndex]!),
       ),
     );
   }
